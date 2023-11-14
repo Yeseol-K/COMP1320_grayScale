6abcf24487d4b1408674353bf9bcc9a845c680d1
unknown <ye37seol@gmailcom>
2023-11-14 01:03:33 -0800

    first

---
+++ IOhandler.js
@@ -0,0 +1,93 @@
+/*
+ * Project: Milestone 1
+ * File Name: IOhandler.js
+ * Description: Collection of functions for files input/output related operations
+ *
+ * Created Date:
+ * Author:
+ *
+ */
+
+const unzipper = require("unzipper"),
+  fs = require("fs/promises"),
+  { createReadStream, createWriteStream} = require("fs"),
+  PNG = require("pngjs").PNG,
+  path = require("path");
+  const AdmZip = require("adm-zip");
+
+/**
+ * Description: decompress file from given pathIn, write to given pathOut
+ *
+ * @param {string} pathIn
+ * @param {string} pathOut
+ * @return {promise}
+ */
+const unzip = (pathIn, pathOut) => {
+  try {
+    const zip = new AdmZip(pathIn);
+    zip.extractAllTo(pathOut, true);
+    console.log("Extraction operation complete");
+  } catch (error) {
+      console.log(error.message);
+  }
+};
+
+/**
+ * Description: read all the png files from given directory and return Promise containing array of each png file path
+ *
+ * @param {string} path
+ * @return {promise}
+ */
+const readDir = (dir) => {
+  return fs.readdir(dir).then((files) => {
+    let arr = [];
+    files.forEach(file => {
+      if (path.extname(file) === ".png") {
+        arr.push(path.join(dir, file));
+      }
+    }) 
+    return arr;
+  })
+}
+/**
+ * Description: Read in png file by given pathIn,
+ * convert to grayscale and write to given pathOut
+ *
+ * @param {string} filePath
+ * @param {string} pathProcessed
+ * @return {promise}
+ */
+const grayScale = (filePath, pathProcessed) => {
+  return new Promise ((resolve, reject) => {
+    createReadStream(filePath)
+    .pipe(new PNG())
+    .on("parsed", function () {
+      for (var y = 0; y < this.height; y++) {
+        for (var x = 0; x < this.width; x++) {
+          var idx = (this.width * y + x) << 2;
+          const r = this.data[idx];
+          const g = this.data[idx + 1];
+          const b = this.data[idx + 2];
+    
+          const gray = (r+g+b)/3;
+    
+          this.data[idx] = gray;
+          this.data[idx + 1] = gray;
+          this.data[idx + 2] = gray;
+        }
+      }
+      this.pack().pipe(createWriteStream(pathProcessed))
+        .on("finish", () => resolve())
+        .on("error", (err) => reject(err));
+      });
+  }); 
+};
+
+
+
+module.exports = {
+  unzip,
+  readDir,
+  grayScale,
+};
+
---
+++ main.js
@@ -0,0 +1,31 @@
+const path = require("path");
+
+/*
+ * Project: Milestone 1
+ * File Name: main.js
+ * Description:
+ *
+ * Created Date:
+ * Author:
+ *
+ */
+
+const IOhandler = require("./IOhandler");
+const zipFilePath = path.join(__dirname, "myfile.zip");
+const pathUnzipped = path.join(__dirname, "unzipped");
+const pathProcessed = path.join(__dirname, "grayscaled");
+
+IOhandler.unzip(zipFilePath, pathUnzipped) //sync doesn't need promise chain
+IOhandler.readDir(pathUnzipped)
+.then((imgs) => { 
+  let promArr = [];
+    imgs.forEach((i, index) => {
+      const grayImg = path.join(__dirname, "grayscaled", `in_${index}.png`);
+      promArr.push(IOhandler.grayScale(i, grayImg));
+    })
+    Promise.all([
+      promArr
+    ]).then(() => console.log("All images done"))
+      .catch(err => console.log(err))
+    })
+  
